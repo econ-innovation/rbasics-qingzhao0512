a1f66eb06dacc178570efa82e0da0b57edabda03
qingzhao0512 <qingzhao0512@gmail.com>
2023-12-06 20:08:17 +0800

    第一次作业提交

---
+++ hw1.R
@@ -0,0 +1,14 @@
+
+# 课堂练习1
+## 如何将3.141592657 转换成 character
+as.character(3.141592657)
+
+## 如何将3.141592657 转换成 logical
+as.logical(3.141592657)
+
+
+## 如何判断"hello world"是否是一个character
+is.character("hello world")
+
+## 如何判断一个NA是否是缺失值
+is.na()
